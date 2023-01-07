151129a7ed1f6345a040bb29a107c52095c68299
Javokhirs <qy104542@gmail.com>
2023-01-07 20:18:23 +0500

    init main project

--- untitled/src/Main.java
+++ untitled/src/Main.java
@@ -1,6 +1,12 @@
 public class Main {
     public static void main(String[] args){
         System.out.println("Hello world");
-
+        System.out.println(sum(5,7));
+    }
+    public static int sum(int a, int b){
+        return a+b;
+    }
+    public static int timing(int a, int b){
+        return a*b;
     }
 }
